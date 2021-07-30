56162e0ef434f3f9e58aa9d016d0905d9abd4e5f
COD X BOTZ <88138313+CodXBotz@users.noreply.github.com>
2021-07-30 15:30:08 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -30,7 +30,7 @@ This is a Telegram Rename Bot!
 --------
 
 Support Group : @CodXBotzSupport
-© @SpEcHlDe @TGBotsZ"""
+© @CodXBotz @SpEcHlDe @TGBotsZ"""
     REPLY_TO_DOC_FOR_RENAME_FILE = "Reply to a Telegram media to `/rename New Name.extension` with custom thumbnail support.."
     ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
     FREE_USER_LIMIT_Q_SZE = """Cannot Process.
